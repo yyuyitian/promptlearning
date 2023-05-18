445b68ac53581e8b6264857799beada0ab177676
yyuyitian <95139110+yyuyitian@users.noreply.github.com>
2023-05-18 08:33:13 +0800

    an property account made by chatgpt

--- iterative.py
+++ iterative.py
@@ -21,46 +21,46 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
-fact_sheet_chair = """
-OVERVIEW
-- Part of a beautiful family of mid-century inspired office furniture, 
-including filing cabinets, desks, bookcases, meeting tables, and more.
-- Several options of shell color and base finishes.
-- Available with plastic back and front upholstery (SWC-100) 
-or full upholstery (SWC-110) in 10 fabric and 6 leather options.
-- Base finish options are: stainless steel, matte black, 
-gloss white, or chrome.
-- Chair is available with or without armrests.
-- Suitable for home or business settings.
-- Qualified for contract use.
+# fact_sheet_chair = """
+# OVERVIEW
+# - Part of a beautiful family of mid-century inspired office furniture, 
+# including filing cabinets, desks, bookcases, meeting tables, and more.
+# - Several options of shell color and base finishes.
+# - Available with plastic back and front upholstery (SWC-100) 
+# or full upholstery (SWC-110) in 10 fabric and 6 leather options.
+# - Base finish options are: stainless steel, matte black, 
+# gloss white, or chrome.
+# - Chair is available with or without armrests.
+# - Suitable for home or business settings.
+# - Qualified for contract use.
 
-CONSTRUCTION
-- 5-wheel plastic coated aluminum base.
-- Pneumatic chair adjust for easy raise/lower action.
+# CONSTRUCTION
+# - 5-wheel plastic coated aluminum base.
+# - Pneumatic chair adjust for easy raise/lower action.
 
-DIMENSIONS
-- WIDTH 53 CM | 20.87”
-- DEPTH 51 CM | 20.08”
-- HEIGHT 80 CM | 31.50”
-- SEAT HEIGHT 44 CM | 17.32”
-- SEAT DEPTH 41 CM | 16.14”
+# DIMENSIONS
+# - WIDTH 53 CM | 20.87”
+# - DEPTH 51 CM | 20.08”
+# - HEIGHT 80 CM | 31.50”
+# - SEAT HEIGHT 44 CM | 17.32”
+# - SEAT DEPTH 41 CM | 16.14”
 
-OPTIONS
-- Soft or hard-floor caster options.
-- Two choices of seat foam densities: 
- medium (1.8 lb/ft3) or high (2.8 lb/ft3)
-- Armless or 8 position PU armrests 
+# OPTIONS
+# - Soft or hard-floor caster options.
+# - Two choices of seat foam densities: 
+#  medium (1.8 lb/ft3) or high (2.8 lb/ft3)
+# - Armless or 8 position PU armrests 
 
-MATERIALS
-SHELL BASE GLIDER
-- Cast Aluminum with modified nylon PA6/PA66 coating.
-- Shell thickness: 10 mm.
-SEAT
-- HD36 foam
+# MATERIALS
+# SHELL BASE GLIDER
+# - Cast Aluminum with modified nylon PA6/PA66 coating.
+# - Shell thickness: 10 mm.
+# SEAT
+# - HD36 foam
 
-COUNTRY OF ORIGIN
-- Italy
-"""
+# COUNTRY OF ORIGIN
+# - Italy
+# """
 # prompt = f"""
 # Your task is to help a marketing team create a 
 # description for a retail website of a product based 
@@ -134,34 +134,77 @@ COUNTRY OF ORIGIN
 # response = get_completion(prompt)
 # print(response)
 
-prompt = f"""
-Your task is to help a marketing team create a 
-description for a retail website of a product based 
-on a technical fact sheet.
+# prompt = f"""
+# Your task is to help a marketing team create a 
+# description for a retail website of a product based 
+# on a technical fact sheet.
 
-Write a product description based on the information 
-provided in the technical specifications delimited by 
-triple backticks.
+# Write a product description based on the information 
+# provided in the technical specifications delimited by 
+# triple backticks.
 
-The description is intended for furniture retailers, 
-so should be technical in nature and focus on the 
-materials the product is constructed from.
+# The description is intended for furniture retailers, 
+# so should be technical in nature and focus on the 
+# materials the product is constructed from.
 
-At the end of the description, include every 7-character 
-Product ID in the technical specification.
+# At the end of the description, include every 7-character 
+# Product ID in the technical specification.
 
-After the description, include a table that gives the 
-product's dimensions. The table should have two columns.
-In the first column include the name of the dimension. 
-In the second column include the measurements in inches only.
+# After the description, include a table that gives the 
+# product's dimensions. The table should have two columns.
+# In the first column include the name of the dimension. 
+# In the second column include the measurements in inches only.
 
-Give the table the title 'Product Dimensions'.
+# Give the table the title 'Product Dimensions'.
 
-Format everything as HTML that can be used in a website. 
-Place the description in a <div> element.
+# Format everything as HTML that can be used in a website. 
+# Place the description in a <div> element.
 
-Technical specifications: ```{fact_sheet_chair}```
+# Technical specifications: ```{fact_sheet_chair}```
+# """
+
+personal_property = '''
+负债：980000
+固定资产：1100000
+流动资产：450000
+每月收入：20000
+每月开支：5000
+'''
+
+personal_property_target = '''
+3年内还清贷款，并且储蓄满1000000
+'''
+
+# prompt = f""""
+# 你的任务是帮助分析这个人的财务状况，并给出理财建议。
+
+# 这个人的财务状况是：```{personal_roperty}```
+# """
+
+# prompt = f""""
+# 你的任务是帮助分析这个人的财务状况和财务目标，并给出理财建议。
+
+# 这个人的财务状况是：```{personal_property}```
+# 这个人的财务目标是：```{personal_property_target}```
+# """
+
+# prompt = f""""
+# 你的任务是帮助分析这个人的财务状况和财务目标，并给出理财建议。
+
+# 这个人的财务状况是：```{personal_property}```
+# 这个人的财务目标是：```{personal_property_target}```
+
+# 这个人可以通过网页输入财务状况以及财务目标，然后点击按钮，前端将这些信息发送给后端，请输出html格式的web
+# """
+
+prompt = f""""
+你的任务是帮助写一个web页面，让用户可以输入以下信息：
+
+```{personal_property}```
+```{personal_property_target}```
+
 """
+
 
 response = get_completion(prompt)
 print(response)
